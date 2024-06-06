594d2db26ecdaeaa642b4510a9f4fa76c63682aa
drupaldev143 <119724661+drupaldev143@users.noreply.github.com>
2024-06-06 14:14:10 +0330

    add file base 64 service

--- src/Controller/HttpClientFileUploadController.php
+++ src/Controller/HttpClientFileUploadController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Drupal\http_client_file_upload\Controller;
 
+use Drupal;
 use Drupal\Core\Controller\ControllerBase;
 
 /**
@@ -15,10 +16,11 @@ final class HttpClientFileUploadController extends ControllerBase {
    * Builds the response.
    */
   public function __invoke(): array {
+    $fileUploadBase64 = Drupal::service('http_client_file_upload.base64')->base64();
+    $fileUpload = Drupal::service('http_client_file_upload.upload')
+      ->upload('node', 'article', 'field_image', 'https://srdr10.ddev.site/sites/default/files/2024-06/1.jpg');
 
-    $fileUpload = \Drupal::service('http_client_file_upload.upload')->upload('node', 'article', 'field_image', 'https://srdr10.ddev.site/sites/default/files/2024-06/1.jpg');
-    dump($fileUpload);
-
+    dump($fileUploadBase64);
     $build['content'] = [
       '#type' => 'item',
       '#markup' => $this->t('It works!'),
